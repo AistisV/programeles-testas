c0d68760addfbba84c503e89ee9d5e87985395e5
AistisV <63104044+AistisV@users.noreply.github.com>
2022-03-28 20:27:07 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+
+// $(function() {
+//   $('a[href*=#]').on('click', function(e) {
+//     e.preventDefault();
+//     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+//   });
+// });
